54ed46fee1310b73501bc74dcabe16984140123a
doctor-ricco <ricardoaraujo.vet@hotmail.com>
2025-03-11 11:12:57 +0000

    favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html className="h-full bg-white" lang="en">
       <Head>
+        <link rel="icon" type="image/png" href="/images/logo-fav-icon.png" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       </Head>
       <body className="h-full">
